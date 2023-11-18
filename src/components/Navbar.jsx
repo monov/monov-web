0c6729b43b990c8d65b2c826bb7748276466babf
monov <shohruhulous@gmail.com>
2023-11-18 21:12:14 +0500

    mobile version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Navbar = ({ setActive, active }) => {
   const handleClick = (act) => {
     setActive(act);
     console.log(active);
   };
+  const [hamActive, setHamActive] = useState(false);
+
+  const hamClick = () => {
+    setHamActive((v) => !v);
+    console.log(hamActive);
+  };
 
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ delay: .2 }}
+      transition={{ delay: 0.2 }}
       className="navbar"
     >
       <div className="navbar-buttons-wrapper">
@@ -43,6 +50,14 @@ const Navbar = ({ setActive, active }) => {
         >
           Blog
         </Link>
+      </div>
+      <div className={`hamburger ${hamActive ? "active" : ""}`}>
+        <div onClick={hamClick} className="hamburger-in"></div>
+        <div className={`menu-wrapper ${hamActive ? "active" : ""}`}>
+          <Link onClick={() => {setHamActive(false)}} to="/" className={`menu ${hamActive ? "active" : ""}`}>Home</Link>
+          <Link onClick={() => {setHamActive(false)}} to="/portfolio" className={`menu ${hamActive ? "active" : ""}`}>Portofolio</Link>
+          <Link onClick={() => {setHamActive(false)}} to="/blog" className={`menu ${hamActive ? "active" : ""}`}>Blog</Link>
+        </div>
       </div>
     </motion.div>
   );
